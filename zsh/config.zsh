68f93a3085daed62bd96d148a21dcb43fa6e20f6
kunitomo <kunitomo@kunitomonoMacBook-Air.local>
2023-11-18 00:38:28 +0900

    pecoの設定を追加

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -17,6 +17,9 @@ HISTFILE=$ZDOTDIR/.zsh-history
 HISTSIZE=100000
 SAVEHIST=1000000
 
+## 環境変数を補完
+setopt AUTO_PARAM_KEYS
+
 
 ### alias ###
 
@@ -36,3 +39,44 @@ alias gpl='git pull origin $(git rev-parse --abbrev-ref HEAD)'
 alias dcu='docker-compose up'
 alias dcp='docker-compose ps'
 alias dcd='docker-compose down'
+
+
+# peco
+## コマンド履歴検索 Ctrl-R
+function peco-history-selection() {
+  BUFFER=`history -n 1 | tac  | awk '!a[$0]++' | peco`
+  CURSOR=$#BUFFER
+  zle reset-prompt
+}
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
+
+## コマンド履歴からディレクトリ検索・移動 Ctrl-E
+if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
+  autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+  add-zsh-hook chpwd chpwd_recent_dirs
+  zstyle ':completion:*' recent-dirs-insert both
+  zstyle ':chpwd:*' recent-dirs-default true
+  zstyle ':chpwd:*' recent-dirs-max 1000
+  zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/chpwd-recent-dirs"
+fi
+function peco-cdr () {
+  local selected_dir="$(cdr -l | sed 's/^[0-9]* *//' | peco)"
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+}
+zle -N peco-cdr
+bindkey '^E' peco-cdr
+
+## カレントディレクトリ以下のディレクトリ検索・移動 Ctrl-X
+function find_cd() {
+  local selected_dir=$(find . -type d | peco)
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+}
+zle -N find_cd
+bindkey '^X' find_cd
