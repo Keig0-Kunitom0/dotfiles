88fa05424ec009201fc5c2af0c2e25ec7c5db4dd
kunitomo <kunitomo@kunitomonoMacBook-Air.local>
2023-11-18 00:41:06 +0900

    サジェストのカラーを変更

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,6 +6,9 @@ zinit light zdharma/fast-syntax-highlighting
 
 # コマンド履歴からコマンドをサジェストする
 zinit light zsh-users/zsh-autosuggestions
+# サジェストのカラーを変更
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=244"
 
 # git open で現在のgitレポジトリをブラウザで開いてくれる
 zinit light paulirish/git-open
+
