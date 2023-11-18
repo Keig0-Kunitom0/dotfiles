efc9d8dcb2386b120c8b65cdadec189e014a3c62
k_kunitomo <k_kunitomo@ga-tech.co.jp>
2023-11-18 15:50:15 +0900

    treesitterパクる

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,10 +6,45 @@ return {
     local nvim_treesitter = require("nvim-treesitter")
     -- configure nvim-tree
     nvim_treesitter.setup({
-      ensure_installed = 'all',
       highlight = { enable = true },
+      -- enable indentation
       indent = { enable = true },
-      disable = {}
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "ruby",
+        "lua"
+      },
+      -- auto install above language parsers
+      auto_install = true,
     })
+
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.erb = {
+      install_info = {
+        url = "https://github.com/tree-sitter/tree-sitter-embedded-template", -- local path or git repo
+        files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+        -- optional entries:
+        branch = "master", -- default branch in case of git repo if different from master
+        requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+      },
+      filetype = "erb", -- if filetype does not match the parser name
+    }
   end
 }
