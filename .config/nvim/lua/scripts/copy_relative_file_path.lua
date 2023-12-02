cf81f2e65bf44c43925b788fc023386291c6f95d
kunitomo <kunitomo@kunitomonoMacBook-Air.local>
2023-12-02 21:40:08 +0900

    ファイルパスコピースクリプトを修正

--- .config/nvim/lua/scripts/copy_relative_file_path.lua
+++ .config/nvim/lua/scripts/copy_relative_file_path.lua
@@ -4,8 +4,8 @@ function copy_file_path.copy_relative_file_path()
   local absolute_file_path = vim.api.nvim_buf_get_name(0)
   local git_path = vim.fn.system("git rev-parse --show-toplevel")
 
-  local relative_file_path = "/" .. string.sub(absolute_file_path, git_path:len() + 1)
-  local copy_command = "echo " .. "\"." .. relative_file_path .. "\"" .. " | pbcopy"
+  local relative_file_path = string.sub(absolute_file_path, git_path:len() + 1)
+  local copy_command = "echo " .. "\"" .. relative_file_path .. "\"" .. " | pbcopy"
   vim.fn.system(copy_command)
   print(relative_file_path)
   print("command:" .. copy_command)
