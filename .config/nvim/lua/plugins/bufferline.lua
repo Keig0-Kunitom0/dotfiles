0ffadf42af508eed3385e7d300a29f0851a646a0
kunitomo <kunitomo@kunitomonoMacBook-Air.local>
2023-12-04 00:22:19 +0900

    カラースキーム変更

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,32 +1,32 @@
 return {
+  event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
   'akinsho/bufferline.nvim',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
   config = function()
     vim.keymap.set('n', '<C-.>', '<Cmd>BufferLineCycleNext<CR>', {})
     vim.keymap.set('n', '<C-,>', '<Cmd>BufferLineCyclePrev<CR>', {})
     vim.keymap.set('n', '<leader>q', '<CMD>BufferLinePickClose<CR>')
     vim.keymap.set('n', '<leader>wq', '<CMD>BufferLineCloseOthers<CR>')
     local bufferline = require('bufferline')
-    require("bufferline").setup {
+    require("bufferline").setup({
       options = {
         mode = "buffers",
         style_preset = bufferline.style_preset.minimal,
         always_show_bufferline = true,
         show_buffer_close_icons = false,
         show_tab_indicators = true,
-        separator_style = "slope",
+        separator_style = {'', ''},
         show_close_icon = false,
         color_icons = true,
         diagnostics = "nvim_lsp",
         offsets = {
           {
             filetype = "NvimTree",
-            text = "File Explorer",
-            text_align = "left",
+            text = "Explorer",
+            text_align = "center"
           }
         }
       }
-    }
+    })
   end
 }
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,21 @@
+return {
+  event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
+  "arturgoms/moonbow.nvim",
+  config = function()
+    vim.cmd([[colorscheme moonbow]])
+  end
+}
+
+-- return {
+--   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function()
+--     vim.o.background = "dark" -- or "light" for light mode
+--     vim.cmd([[colorscheme gruvbox]])
+--   end
+-- }
+
 -- return {
 --   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
 --   'sjl/badwolf',
@@ -6,26 +24,15 @@
 --   end
 -- }
 
-return {
-	{
-    event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
-		"craftzdog/solarized-osaka.nvim",
-		priority = 1000,
-		config = function()
-      vim.cmd("colorscheme solarized-osaka")
-		end
-	}
-}
-
 -- return {
---   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
---   "ellisonleao/gruvbox.nvim",
---   priority = 1000 ,
---   config = function()
---     vim.o.background = "dark" -- or "light" for light mode
---     vim.cmd([[let g:gruvbox_contrast_dark='hard']])
---     vim.cmd([[colorscheme gruvbox]])
---   end
+-- 	{
+--     event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
+-- 		"craftzdog/solarized-osaka.nvim",
+-- 		priority = 1000,
+-- 		config = function()
+--       vim.cmd("colorscheme solarized-osaka")
+-- 		end
+-- 	}
 -- }
 
 -- return {
