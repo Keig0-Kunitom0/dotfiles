5069054b738a4f0e23ebdbf6b265c6245cdb02dc
k_kunitomo <k_kunitomo@ga-tech.co.jp>
2023-12-14 12:27:07 +0900

    alpha導入

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local logo = {
+        "            :h-                                  Nhy`               ",
+        "           -mh.                           h.    `Ndho               ",
+        "           hmh+                          oNm.   oNdhh               ",
+        "          `Nmhd`                        /NNmd  /NNhhd               ",
+        "          -NNhhy                      `hMNmmm`+NNdhhh               ",
+        "          .NNmhhs              ```....`..-:/./mNdhhh+               ",
+        "           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
+        "           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
+        "      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
+        " .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
+        " h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
+        " hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
+        " /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
+        "  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
+        "   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
+        "     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
+        "       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
+        "       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
+        "       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
+        "       //+++//++++++////+++///::--                 .::::-------::   ",
+        "       :/++++///////////++++//////.                -:/:----::../-   ",
+        "       -/++++//++///+//////////////               .::::---:::-.+`   ",
+        "       `////////////////////////////:.            --::-----...-/    ",
+        "        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
+        "         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
+        "           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
+        "            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
+        "           s-`::--:::------:////----:---.-:::...-.....`./:          ",
+        "          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
+        "         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
+        "        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
+        "                        .-:mNdhh:.......--::::-`                    ",
+        "                           yNh/..------..`                          ",
+        "                                                                    ",
+      }
+
+      dashboard.section.header.val = logo
+      dashboard.section.buttons.val = {
+        dashboard.button("l", " ☑️   Lazy Git", ":lua _lazygit_toggle()<CR>"),
+        dashboard.button("r", " 🚀  Recent File", ":Telescope oldfiles<CR>"),
+        dashboard.button("f", " 🎄  Find File", ":Telescope find_files<CR>"),
+        dashboard.button('g', " 📗  Find Text", ':Telescope live_grep <CR>'),
+        dashboard.button('n', " 📄  New File", ':ene <BAR> startinsert <CR>'),
+        dashboard.button("q", " 🚪  Quit Nvim", ":qa<CR>"),
+      }
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      require("alpha").setup(dashboard.opts)
+      vim.api.nvim_create_autocmd("User", {
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime * 100) / 100
+          dashboard.section.footer.val = "󱐌 Lazy-loaded "
+          .. stats.loaded
+          .. "/"
+          .. stats.count
+          .. " plugins in "
+          .. ms
+          .. "ms"
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
+    end,
+  },
+}
