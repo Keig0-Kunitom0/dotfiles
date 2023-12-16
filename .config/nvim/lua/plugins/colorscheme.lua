759932a85ed4186fdc83d5dc7172210d7197eea0
k_kunitomo <k_kunitomo@ga-tech.co.jp>
2023-12-16 11:26:29 +0900

    編集

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
--- return {
---   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
---   "arturgoms/moonbow.nvim",
---   config = function()
---     vim.cmd([[colorscheme moonbow]])
---   end
--- }
+return {
+  event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
+  "arturgoms/moonbow.nvim",
+  config = function()
+    vim.cmd([[colorscheme moonbow]])
+  end
+}
 
 -- return {
 --   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
@@ -49,17 +49,17 @@
 --   end
 -- }
 
-return {
-  event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
-  "rockyzhang24/arctic.nvim",
-  dependencies = { "rktjmp/lush.nvim" },
-  name = "arctic",
-  branch = "main",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme arctic")
-  end
-}
+-- return {
+--   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
+--   "rockyzhang24/arctic.nvim",
+--   dependencies = { "rktjmp/lush.nvim" },
+--   name = "arctic",
+--   branch = "main",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd("colorscheme arctic")
+--   end
+-- }
 
 -- return {
 -- 	{
@@ -70,4 +70,31 @@ return {
 --       vim.cmd("colorscheme solarized-osaka")
 -- 		end
 -- 	}
+-- }
+-- return {
+--   'ray-x/aurora',
+--   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
+--   config = function()
+--     vim.cmd([[
+--       set termguicolors
+--       let g:aurora_italic = 1
+--       let g:aurora_transparent = 1
+--       let g:aurora_bold = 1
+--       let g:aurora_darker = 1
+--     ]])
+--     vim.cmd('colorscheme aurora')
+--     vim.api.nvim_set_hl(0, '@string', {fg='#59E343'})
+--     vim.api.nvim_set_hl(0, '@field', {fg='#f93393'})
+--     vim.api.nvim_set_hl(0, '@number', {fg='#e933e3'})
+--   end
+-- }
+
+-- return {
+--   "folke/tokyonight.nvim",
+--   event = "VimEnter", -- neovim起動時にプラグインが読み込まれるようにする
+--   priority = 10000,
+--   opts = {},
+--   config = function()
+--     vim.cmd("colorscheme tokyonight-night")
+--   end,
 -- }
